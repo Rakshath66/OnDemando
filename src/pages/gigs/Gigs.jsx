2bfd3ffd8eb46fc7001070fafa2e61e43f4c1936
Rakshath U Shetty <eng20cs0278.rakshath.u.shetty@gmail.com>
2024-03-19 23:15:03 +0530

    css

--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -1,23 +1,49 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./Gigs.scss";
-import { gigs } from "../../data";
-import GigCard from "../../components/gigCard/GigCard";
 
+import GigCard from "../../components/gigCard/GigCard";
+import { useQuery } from "@tanstack/react-query";
+import newRequest from "../../utils/newRequest";
+import { useLocation } from "react-router-dom";
+
+//show gigs, with filters
 function Gigs() {
-  const [sort, setSort] = useState("sales");//if open, if sares, show created at, else vise versa
+  const [sort, setSort] = useState("sales"); // if open, if sares, show created at, else vise versa
   const [open, setOpen] = useState(false);
   const minRef = useRef();
   const maxRef = useRef();
+
+  const { search } = useLocation();
+
+  // fetch with filters and store in gigs
+  const { isLoading, error, data, refetch } = useQuery({
+    queryKey: ["gigs"],
+    queryFn: () =>
+      newRequest
+        .get(
+          `/gigs${search}&min=${minRef.current.value}&max=${maxRef.current.value}&sort=${sort}`
+        )
+        .then((res) => {
+          return res.data;
+        }),
+  });
+
+  console.log(data);
 
   const reSort = (type) => {
     setSort(type);
     setOpen(false);
   };
 
-  const apply = ()=>{
-    console.log(minRef.current.value)
-    console.log(maxRef.current.value)
-  }
+  //if change in sort(newest, best selling), refetch the data
+  useEffect(() => {
+    refetch();
+  }, [sort]);
+
+  //on apply, refetch data
+  const apply = () => {
+    refetch();
+  };
 
   return (
     <div className="gigs">
@@ -48,17 +74,20 @@ function Gigs() {
                   <span onClick={() => reSort("createdAt")}>Newest</span>
                 ) : (
                   <span onClick={() => reSort("sales")}>Best Selling</span>
-                  )}
-                  <span onClick={() => reSort("sales")}>Popular</span>
+                )}
+                <span onClick={() => reSort("sales")}>Popular</span>
               </div>
             )}
           </div>
         </div>
 
+        {/* if loading and no error, show all gigs */}
         <div className="cards">
-          {gigs.map((gig) => (
-            <GigCard key={gig.id} item={gig} />
-          ))}
+          {isLoading
+            ? "loading"
+            : error
+            ? "Something went wrong!"
+            : data.map((gig) => <GigCard key={gig._id} item={gig} />)}
         </div>
       </div>
     </div>
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,10 +1,51 @@
-import React from "react"
-import "./Login.scss"
+import React, { useState } from "react";
+import "./Login.scss";
+import newRequest from "../../utils/newRequest";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    //req to server with username and pass, recieve token and user info, store user info in localstorage, navigate to home page
+    try {
+      const res = await newRequest.post("/auth/login", { username, password });//to get cookies(tokens), add ,{withCredentials: true} //otherwise get only user info
+      localStorage.setItem("currentUser", JSON.stringify(res.data));
+      navigate("/")
+      // console.log(res.data);
+    } catch (err) {
+      setError(err.response.data);
+    }
+  };
+
   return (
-    <div>Login</div>
-  )
+    <div className="login">
+      <form onSubmit={handleSubmit}>
+        <h1>Sign in</h1>
+        <label htmlFor="">Username</label>
+        <input
+          name="username"
+          type="text"
+          placeholder="johndoe"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <label htmlFor="">Password</label>
+        <input
+          name="password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+        {error && error}
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
